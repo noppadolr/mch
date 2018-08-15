7dc158b9d28f995ceb147e8bef69b6eb66ea1e97
riyasarn <riyasarn.n@gmail.com>
2018-08-15 09:19:22 +0700

    first time

---
+++ frontend/views/site/index.php
@@ -0,0 +1,82 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'ระบบติดตามข้อมูล งานอนามัยแม่และเด็ก';
+?>
+<div class="site-index">
+    <div class="body-content">
+
+            <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12">
+
+                    <!-- Apply any bg-* class to to the info-box to color it -->
+                    <div class="info-box bg-fuchsia">
+                <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
+                <div class="info-box-content">
+                <span class="info-box-text">รายใหม่</span>
+                <span class="info-box-number">410</span>
+                <!-- The progress section is optional -->
+                <div class="progress">
+                    <div class="progress-bar" style="width: 100%"></div>
+                </div>
+                <span class="progress-description">
+                    จำนวนหญิงตั้งครรภ์รายใหม่ในปี
+                </span>
+                </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+            </div>  <!-- col -->
+
+            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-green">
+              <span class="info-box-icon"><i class="fa fa-thumbs-o-up"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">จำนวนคลอด</span>
+                <span class="info-box-number">414</span>
+                <div class="progress">
+                  <div class="progress-bar" style="width: 100%"></div>
+                </div>
+                <span class="progress-description">
+                  จำนวนหญิงคลอดในปี
+                </span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </div><!-- /.col -->
+
+            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-aqua">
+              <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">ยังไม่คลอด</span>
+                <span class="info-box-number">41,410</span>
+                <div class="progress">
+                  <div class="progress-bar" style="width: 100%"></div>
+                </div>
+                <span class="progress-description">
+                  หญิงตั้งครรภ์ที่ยังไม่คลอด
+                </span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </div><!-- /.col -->
+
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-yellow">
+              <span class="info-box-icon"><i class="fa fa-calendar "></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">แท้ง</span>
+                <span class="info-box-number">41,410</span>
+                <div class="progress">
+                  <div class="progress-bar" style="width: 100%"></div>
+                </div>
+                <span class="progress-description">
+                  จำนวนแท้งทั้งหมด
+                </span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </div><!-- /.col -->
+
+
+
+            </div>
+    </div>
+</div>
